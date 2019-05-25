ed3384556a7c3a9b751255b581eb4a3e0cd56891
leizhao <leizhao@jieyuechina.com>
2019-05-25 18:07:04 +0800

    增加多选框

--- src/main/webapp/manage/js/user.js
+++ src/main/webapp/manage/js/user.js
@@ -69,6 +69,21 @@ $(function () {
     $('#addUserBtn').click(function () {
         window.location.href = 'user_add.html';
     });
+
+    // 点击编辑跳转到编辑页面
+    $('#updateUserBtn').click(function () {
+        //TODO　记录IDS
+        var allVal = $("input:checkbox:checked").map(function(index,elem) {
+            return $(elem).val();
+        }).get().join(',');
+
+        alert(allVal);
+        var ids = new Array();
+        for(var i=0;i<selected.length;i++){
+            ids[i] = selected[i].id;
+        }
+        window.location.href = 'user_update.html';
+    });
     // 点击编辑跳转到编辑页面
     $('#submitBtn_infoUser').click(function () {
         window.location.href = 'user.html';
@@ -289,6 +304,14 @@ function addUser(){
             $("#realCard")[0].style.border="1px solid red";
             showMsg('.error-msg', "请输入正确身份证号");
             return false;
+        }
+        //如果在职状态是离职、工作未满七天、未满七已处理，则必须填写离职日期
+        if(status==2||status==4||status==5){
+            if(leaveDate==null||leaveDate==''){
+                $("#leaveDate")[0].style.border="1px solid red";
+                showMsg('.error-msg', "请输入离职日期");
+                return false;
+            }
         }
         var obj = {
             "id":localStorage.getItem('id'),
@@ -680,7 +703,8 @@ function queryUserList(roleId,userName,page){
                 if(list.length>0) {
                     $.each(list, function (i) {
                         var _tr = $("<tr>");
-                        var str =  '         <td>' + parseInt((page - 1) * 10 +i+1) + '</td>' +
+                        var str =  '         <td><input type="checkbox" name="checkItem" value="'+this.id+'"/></td>      ' +
+                            '                <td>' + parseInt((page - 1) * 10 +i+1) + '</td>' +
                             '                <td>' + this.name + '</td>' +
                             '                <td>' + sexAction(this.sex) + '</td>' +
                             '                <td>' + GetAge(this.idCard) + '</td>' +
@@ -704,6 +728,7 @@ function queryUserList(roleId,userName,page){
                         _tr.html(str).data(list[i]);
                         $("#userTable tbody").append(_tr);
                     });
+                    initTableCheckbox();
                 }else{
                     $("#userTable tbody").append('<tr class="table_null"><td style="padding-left: 15px; text-align: left;" colspan=\'15\'>暂无数据</td></tr>');
                 }
@@ -1112,4 +1137,46 @@ function validateIdCard(idCard) {
         //alert("身份证格式不正确!");
         return false;
     }
+
+}
+function initTableCheckbox() {
+    var $thr = $('table thead tr');
+    // var $checkAllTh = $('<th><input type="checkbox" id="checkAll" name="checkAll" /></th>');
+    // /*将全选/反选复选框添加到表头最前，即增加一列*/
+    // $thr.prepend($checkAllTh);
+    /*“全选/反选”复选框*/
+    var $checkAll = $thr.find('input');
+    $checkAll.click(function(event){
+        /*将所有行的选中状态设成全选框的选中状态*/
+        $tbr.find('input').prop('checked',$(this).prop('checked'));
+        /*并调整所有选中行的CSS样式*/
+        if ($(this).prop('checked')) {
+            $tbr.find('input').parent().parent().addClass('warning');
+        } else{
+            $tbr.find('input').parent().parent().removeClass('warning');
+        }
+        /*阻止向上冒泡，以防再次触发点击操作*/
+        event.stopPropagation();
+    });
+    /*点击全选框所在单元格时也触发全选框的点击操作*/
+    $checkAllTh.click(function(){
+        $(this).find('input').click();
+    });
+    var $tbr = $('table tbody tr');
+    // var $checkItemTd = $('<td><input type="checkbox" name="checkItem" /></td>');
+    // /*每一行都在最前面插入一个选中复选框的单元格*/
+    // $tbr.prepend($checkItemTd);
+    /*点击每一行的选中复选框时*/
+    $tbr.find('input').click(function(event){
+        /*调整选中行的CSS样式*/
+        $(this).parent().parent().toggleClass('warning');
+        /*如果已经被选中行的行数等于表格的数据行数，将全选框设为选中状态，否则设为未选中状态*/
+        $checkAll.prop('checked',$tbr.find('input:checked').length == $tbr.length ? true : false);
+        /*阻止向上冒泡，以防再次触发点击操作*/
+        event.stopPropagation();
+    });
+    /*点击每一行时也触发该行的选中操作*/
+    $tbr.click(function(){
+        $(this).find('input').click();
+    });
 }
